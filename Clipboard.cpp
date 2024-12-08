994cf3278d6ac04988e64011788ee9d2877d6725
alonefact <104738034+alonefact@users.noreply.github.com>
2024-12-07 22:43:45 -0400

    Update Clipboard.cpp

--- Clipboard.cpp
+++ Clipboard.cpp
@@ -1,21 +1,15 @@
 #include "Clipboard.h"
-#include <windows.h>
-#include <iostream>
-
-// Annoying error handling because it randomly errors??
 
 namespace Clipboard {
 
     void Copy(const std::wstring& text) {
         HGLOBAL hGlob = GlobalAlloc(GMEM_MOVEABLE, (text.size() + 1) * sizeof(wchar_t));
         if (!hGlob) {
-            std::cerr << "GlobalAlloc failed!" << std::endl;
             return;
         }
 
         wchar_t* pText = static_cast<wchar_t*>(GlobalLock(hGlob));
         if (!pText) {
-            std::cerr << "GlobalLock failed!" << std::endl;
             GlobalFree(hGlob);
             return;
         }
@@ -26,14 +20,13 @@ namespace Clipboard {
         if (OpenClipboard(NULL)) {
             EmptyClipboard();
             if (SetClipboardData(CF_UNICODETEXT, hGlob) == NULL) {
-                std::cerr << "SetClipboardData failed!" << std::endl;
                 GlobalFree(hGlob);
             }
             CloseClipboard();
         }
         else {
-            std::cerr << "OpenClipboard failed!" << std::endl;
             GlobalFree(hGlob);
         }
     }
+
 }
