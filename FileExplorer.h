82637ad4ee2370683234c6c408ed4b1693269824
alonefact <104738034+alonefact@users.noreply.github.com>
2024-12-07 22:37:30 -0400

    Update FileExplorer.h

--- FileExplorer.h
+++ FileExplorer.h
@@ -1,5 +1,12 @@
 #pragma once
 #include <string>
+#include <string>
+#include <memory>
+#include <vector>
+#include "Random.h"
+#include <windows.h>
+#include <commdlg.h>
+#include "FileExplorer.h"
 
 namespace FileExplorer {
     std::wstring Search();
