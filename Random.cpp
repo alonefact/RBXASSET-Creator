7946fa95e2becc5a178782e915754481060e2298
alonefact <104738034+alonefact@users.noreply.github.com>
2024-12-07 22:56:18 -0400

    Update Random.cpp

--- Random.cpp
+++ Random.cpp
@@ -1,14 +1,13 @@
 #include "Random.h"
-#include <random>
 
 namespace Random {
 
-    std::wstring New(size_t Length) {
-        const wchar_t CharSet[] = L"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    std::string New(size_t Length) {
+        const char CharSet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         std::uniform_int_distribution<> Dist(0, sizeof(CharSet) / sizeof(CharSet[0]) - 2);
         std::mt19937 Rng{ std::random_device{}() };
 
-        std::wstring RandomString;
+        std::string RandomString;
         for (size_t i = 0; i < Length; ++i) {
             RandomString += CharSet[Dist(Rng)];
         }
