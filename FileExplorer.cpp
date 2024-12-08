83ec137bde0526c148f4bd9fb7db3160e101a4a4
alonefact <104738034+alonefact@users.noreply.github.com>
2024-12-07 22:36:39 -0400

    Update FileExplorer.cpp

--- FileExplorer.cpp
+++ FileExplorer.cpp
@@ -1,66 +1,82 @@
 #include "FileExplorer.h"
-#include "Random.h"
-#include <windows.h>
-#include <commdlg.h>
-#include <string>
-#include <random>
 
 namespace FileExplorer {
-    std::wstring Search() {
-        wchar_t FileName[MAX_PATH] = L"";
-        HWND HwndOwner = CreateWindowEx(WS_EX_TOPMOST, L"STATIC", L"File Explorer", WS_POPUP, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, NULL, NULL);
 
+    std::wstring Search() {
+        auto WindowDeleter = [](HWND hwnd) { if (hwnd) DestroyWindow(hwnd); };
+        std::unique_ptr<std::remove_pointer<HWND>::type, decltype(WindowDeleter)> OwnerWindow(
+            CreateWindowExW(
+                WS_EX_TOPMOST, L"STATIC", L"File Explorer", WS_POPUP,
+                CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+                NULL, NULL, NULL, NULL
+            ), WindowDeleter
+        );
+
+        wchar_t FileName[MAX_PATH] = L"";
         OPENFILENAMEW Ofn = { 0 };
         Ofn.lStructSize = sizeof(Ofn);
-        Ofn.hwndOwner = HwndOwner;
+        Ofn.hwndOwner = OwnerWindow.get();
         Ofn.lpstrFile = FileName;
         Ofn.nMaxFile = MAX_PATH;
         Ofn.lpstrFilter = L"All Files\0*.*\0";
         Ofn.lpstrTitle = L"Select a file";
         Ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
 
-        if (GetOpenFileNameW(&Ofn)) {
-            std::wstring SelectedFile = Ofn.lpstrFile;
-            HANDLE FileHandle = CreateFileW(SelectedFile.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+        if (!GetOpenFileNameW(&Ofn))
+            return L"";
 
-            if (FileHandle != INVALID_HANDLE_VALUE) {
-                wchar_t UserPath[MAX_PATH];
-                if (GetEnvironmentVariableW(L"USERPROFILE", UserPath, MAX_PATH)) {
-                    std::wstring BasePath = std::wstring(UserPath) + L"\\AppData\\Local\\Roblox\\Versions\\";
-                    WIN32_FIND_DATAW FindData;
-                    HANDLE hFind = FindFirstFileW((BasePath + L"version-*").c_str(), &FindData);
+        std::wstring SelectedFile = Ofn.lpstrFile;
 
-                    if (hFind != INVALID_HANDLE_VALUE) {
-                        do {
-                            if (FindData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
-                                std::wstring VersionFolderPath = BasePath + FindData.cFileName + L"\\";
-                                std::wstring ExePath = VersionFolderPath + L"RobloxStudioBeta.exe";
+        auto HandleDeleter = [](HANDLE handle) { if (handle != INVALID_HANDLE_VALUE && handle != NULL) CloseHandle(handle); };
+        std::unique_ptr<std::remove_pointer<HANDLE>::type, decltype(HandleDeleter)> File(
+            CreateFileW(
+                SelectedFile.c_str(),
+                GENERIC_READ,
+                FILE_SHARE_READ,
+                NULL,
+                OPEN_EXISTING,
+                FILE_ATTRIBUTE_NORMAL,
+                NULL
+            ), HandleDeleter
+        );
 
-                                if (GetFileAttributesW(ExePath.c_str()) != INVALID_FILE_ATTRIBUTES) {
-                                    std::wstring ContentFolderPath = VersionFolderPath + L"content\\";
-                                    if (GetFileAttributesW(ContentFolderPath.c_str()) != INVALID_FILE_ATTRIBUTES) {
-                                        std::wstring RandomPrefix = Random::New(100);
-                                        std::wstring DestinationPath = ContentFolderPath + RandomPrefix +
-                                                                        SelectedFile.substr(SelectedFile.find_last_of(L"."));
+        if (File.get() == INVALID_HANDLE_VALUE)
+            return L"";
 
-                                        if (CopyFileW(SelectedFile.c_str(), DestinationPath.c_str(), FALSE)) {
-                                            FindClose(hFind);
-                                            CloseHandle(FileHandle);
-                                            DestroyWindow(HwndOwner);
-                                            return DestinationPath;
-                                        }
-                                    }
-                                }
-                            }
-                        } while (FindNextFileW(hFind, &FindData));
-                        FindClose(hFind);
-                    }
-                }
-                CloseHandle(FileHandle);
-            }
-        }
+        wchar_t UserPath[MAX_PATH];
+        if (!GetEnvironmentVariableW(L"USERPROFILE", UserPath, MAX_PATH))
+            return L"";
+        std::wstring BasePath = std::wstring(UserPath) + L"\\AppData\\Local\\Roblox\\Versions\\";
 
-        DestroyWindow(HwndOwner);
-        return L"";
+        WIN32_FIND_DATAW FindData;
+        HANDLE HFind = FindFirstFileW((BasePath + L"version-*").c_str(), &FindData);
+        if (HFind == INVALID_HANDLE_VALUE)
+            return L"";
+
+        std::wstring DestinationPath;
+        do {
+            if (!(FindData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
+                continue;
+
+            std::wstring VersionPath = BasePath + FindData.cFileName + L"\\";
+            std::wstring ExePath = VersionPath + L"RobloxStudioBeta.exe";
+            std::wstring ContentPath = VersionPath + L"content\\";
+
+            if (GetFileAttributesW(ExePath.c_str()) == INVALID_FILE_ATTRIBUTES ||
+                GetFileAttributesW(ContentPath.c_str()) == INVALID_FILE_ATTRIBUTES)
+                continue;
+
+            std::wstring Extension = SelectedFile.substr(SelectedFile.find_last_of(L"."));
+            std::wstring RandomPrefix = Random::New(100);
+            DestinationPath = ContentPath + RandomPrefix + Extension;
+
+            if (CopyFileW(SelectedFile.c_str(), DestinationPath.c_str(), FALSE))
+                break;
+
+        } while (FindNextFileW(HFind, &FindData));
+        FindClose(HFind);
+
+        return DestinationPath.empty() ? L"" : DestinationPath;
     }
+
 }
